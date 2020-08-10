e13165c26e3a64bc08a1d96f674fb6b7bd922b90
Muhammed Fouzan <fouzanpharmd@gmail.com>
2020-08-10 20:35:13 +0530

    added colorHelper for 10 different shades

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColor from "./seedColor";
-
+import { generatePalette } from "./Colorhelpers";
 function App() {
+  console.log(generatePalette(seedColor[4]));
   return (
     <div>
       <Palette {...seedColor[4]} />
